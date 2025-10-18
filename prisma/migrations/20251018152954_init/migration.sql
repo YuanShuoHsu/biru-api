63dd1b2228bc31ef78903f4f5de6f4241d08a015
YuanShuoHsu <ook1725@gmail.com>
2025-10-18 23:32:10 +0800

    refactor: remove migration.sql file and update Table model in Prisma schema for consistency

---
+++ prisma/migrations/20251018152954_init/migration.sql
@@ -0,0 +1,243 @@
+-- CreateEnum
+CREATE TYPE "Provider" AS ENUM ('local', 'google');
+
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('admin', 'manager', 'staff', 'user');
+
+-- CreateTable
+CREATE TABLE "Menu" (
+    "id" TEXT NOT NULL,
+    "key" TEXT NOT NULL,
+    "name" JSONB NOT NULL DEFAULT '{}',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "storeId" TEXT NOT NULL,
+
+    CONSTRAINT "Menu_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "MenuItem" (
+    "id" TEXT NOT NULL,
+    "key" TEXT NOT NULL,
+    "name" JSONB NOT NULL DEFAULT '{}',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "description" JSONB NOT NULL DEFAULT '{}',
+    "imageUrl" TEXT NOT NULL DEFAULT '/images/IMG_4590.jpg',
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "price" INTEGER NOT NULL,
+    "sold" INTEGER NOT NULL DEFAULT 0,
+    "stock" INTEGER,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "menuId" TEXT NOT NULL,
+
+    CONSTRAINT "MenuItem_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "MenuItemOption" (
+    "id" TEXT NOT NULL,
+    "key" TEXT NOT NULL,
+    "name" JSONB NOT NULL DEFAULT '{}',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "multiple" BOOLEAN NOT NULL DEFAULT false,
+    "required" BOOLEAN NOT NULL DEFAULT false,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "menuItemId" TEXT NOT NULL,
+
+    CONSTRAINT "MenuItemOption_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "MenuItemIngredient" (
+    "id" TEXT NOT NULL,
+    "key" TEXT NOT NULL,
+    "name" JSONB NOT NULL DEFAULT '{}',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "unit" JSONB NOT NULL DEFAULT '{}',
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "usage" DECIMAL(10,3) NOT NULL,
+    "menuItemId" TEXT NOT NULL,
+
+    CONSTRAINT "MenuItemIngredient_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "MenuItemOptionChoice" (
+    "id" TEXT NOT NULL,
+    "key" TEXT NOT NULL,
+    "name" JSONB NOT NULL DEFAULT '{}',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "extraCost" INTEGER NOT NULL DEFAULT 0,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "isShared" BOOLEAN NOT NULL DEFAULT false,
+    "sold" INTEGER NOT NULL DEFAULT 0,
+    "stock" INTEGER,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "menuItemOptionId" TEXT NOT NULL,
+
+    CONSTRAINT "MenuItemOptionChoice_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "MenuItemOptionChoiceIngredient" (
+    "id" TEXT NOT NULL,
+    "key" TEXT NOT NULL,
+    "name" JSONB NOT NULL DEFAULT '{}',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "unit" JSONB NOT NULL DEFAULT '{}',
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "usage" DECIMAL(10,3) NOT NULL,
+    "menuItemOptionChoiceId" TEXT NOT NULL,
+
+    CONSTRAINT "MenuItemOptionChoiceIngredient_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" SERIAL NOT NULL,
+    "content" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "published" BOOLEAN DEFAULT false,
+    "title" TEXT NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "authorId" TEXT,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Store" (
+    "id" TEXT NOT NULL,
+    "name" JSONB NOT NULL DEFAULT '{}',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "slug" TEXT NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Store_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Table" (
+    "id" TEXT NOT NULL,
+    "name" JSONB NOT NULL DEFAULT '{}',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "slug" TEXT NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "storeId" TEXT NOT NULL,
+
+    CONSTRAINT "Table_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "email" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL DEFAULT '',
+    "lastName" TEXT NOT NULL DEFAULT '',
+    "password" TEXT NOT NULL DEFAULT '',
+    "photo" TEXT NOT NULL DEFAULT '/images/IMG_4590.jpg',
+    "provider" "Provider" NOT NULL DEFAULT 'local',
+    "role" "Role" NOT NULL DEFAULT 'user',
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "RefreshToken" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "hashed" TEXT NOT NULL,
+    "revoked" BOOLEAN NOT NULL DEFAULT false,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "RefreshToken_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "Menu_storeId_idx" ON "Menu"("storeId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Menu_storeId_key_key" ON "Menu"("storeId", "key");
+
+-- CreateIndex
+CREATE INDEX "MenuItem_menuId_idx" ON "MenuItem"("menuId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "MenuItem_menuId_key_key" ON "MenuItem"("menuId", "key");
+
+-- CreateIndex
+CREATE INDEX "MenuItemOption_menuItemId_idx" ON "MenuItemOption"("menuItemId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "MenuItemOption_menuItemId_key_key" ON "MenuItemOption"("menuItemId", "key");
+
+-- CreateIndex
+CREATE INDEX "MenuItemIngredient_menuItemId_idx" ON "MenuItemIngredient"("menuItemId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "MenuItemIngredient_menuItemId_key_key" ON "MenuItemIngredient"("menuItemId", "key");
+
+-- CreateIndex
+CREATE INDEX "MenuItemOptionChoice_menuItemOptionId_idx" ON "MenuItemOptionChoice"("menuItemOptionId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "MenuItemOptionChoice_menuItemOptionId_key_key" ON "MenuItemOptionChoice"("menuItemOptionId", "key");
+
+-- CreateIndex
+CREATE INDEX "MenuItemOptionChoiceIngredient_menuItemOptionChoiceId_idx" ON "MenuItemOptionChoiceIngredient"("menuItemOptionChoiceId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "MenuItemOptionChoiceIngredient_menuItemOptionChoiceId_key_key" ON "MenuItemOptionChoiceIngredient"("menuItemOptionChoiceId", "key");
+
+-- CreateIndex
+CREATE INDEX "Post_authorId_idx" ON "Post"("authorId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Store_slug_key" ON "Store"("slug");
+
+-- CreateIndex
+CREATE INDEX "Table_storeId_idx" ON "Table"("storeId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Table_storeId_slug_key" ON "Table"("storeId", "slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "RefreshToken_userId_idx" ON "RefreshToken"("userId");
+
+-- AddForeignKey
+ALTER TABLE "Menu" ADD CONSTRAINT "Menu_storeId_fkey" FOREIGN KEY ("storeId") REFERENCES "Store"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "MenuItem" ADD CONSTRAINT "MenuItem_menuId_fkey" FOREIGN KEY ("menuId") REFERENCES "Menu"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "MenuItemOption" ADD CONSTRAINT "MenuItemOption_menuItemId_fkey" FOREIGN KEY ("menuItemId") REFERENCES "MenuItem"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "MenuItemIngredient" ADD CONSTRAINT "MenuItemIngredient_menuItemId_fkey" FOREIGN KEY ("menuItemId") REFERENCES "MenuItem"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "MenuItemOptionChoice" ADD CONSTRAINT "MenuItemOptionChoice_menuItemOptionId_fkey" FOREIGN KEY ("menuItemOptionId") REFERENCES "MenuItemOption"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "MenuItemOptionChoiceIngredient" ADD CONSTRAINT "MenuItemOptionChoiceIngredient_menuItemOptionChoiceId_fkey" FOREIGN KEY ("menuItemOptionChoiceId") REFERENCES "MenuItemOptionChoice"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Table" ADD CONSTRAINT "Table_storeId_fkey" FOREIGN KEY ("storeId") REFERENCES "Store"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "RefreshToken" ADD CONSTRAINT "RefreshToken_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
