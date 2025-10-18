3e090b451f15a39ef6b8af1351c656e2f194c9f6
YuanShuoHsu <ook1725@gmail.com>
2025-10-18 23:36:51 +0800

    refactor: update UserResponseDto to change id type from number to string for UUID compatibility

--- src/users/dto/user-response.dto.ts
+++ src/users/dto/user-response.dto.ts
@@ -4,8 +4,11 @@ import { Provider, Role } from '@prisma/client';
 import { IsEmail, IsNotEmpty, IsUrl } from 'class-validator';
 
 export class UserResponseDto {
-  @ApiProperty({ description: '使用者 ID', example: 1 })
-  id: number;
+  @ApiProperty({
+    description: '使用者 ID',
+    example: '550e8400-e29b-41d4-a716-446655440000',
+  })
+  id: string;
 
   @ApiProperty({
     description: '建立時間',
