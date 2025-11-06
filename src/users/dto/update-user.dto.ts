fa2c9e39f95dff833e39d6ca0407af238f321729
YuanShuoHsu <ook1725@gmail.com>
2025-11-06 16:50:42 +0800

    refactor: update UpdateUserDto to rename fields and add phone number for enhanced user information

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -5,15 +5,15 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsOptional, IsUrl } from 'class-validator';
 
 export class UpdateUserDto {
+  @ApiProperty({ description: '電話國碼', example: '+886' })
+  @IsNotEmpty()
+  @IsOptional()
+  countryCode?: string;
+
   @ApiProperty({ description: '名', example: 'Coffee' })
   @IsNotEmpty()
   @IsOptional()
   firstName?: string;
-
-  @ApiProperty({ description: '姓', example: 'Biru' })
-  @IsNotEmpty()
-  @IsOptional()
-  lastName?: string;
 
   @ApiProperty({
     description: '頭像 URL',
@@ -22,4 +22,17 @@ export class UpdateUserDto {
   @IsOptional()
   @IsUrl()
   image?: string;
+
+  @ApiProperty({ description: '姓', example: 'Biru' })
+  @IsNotEmpty()
+  @IsOptional()
+  lastName?: string;
+
+  @ApiProperty({
+    description: '聯絡電話',
+    example: '0912345678',
+  })
+  @IsNotEmpty()
+  @IsOptional()
+  phone?: string;
 }
