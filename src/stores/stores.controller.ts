e10e0e6aa95edfa732db9050ba534e1390d342c3
YuanShuoHsu <ook1725@gmail.com>
2026-01-29 13:42:50 +0800

    feat: 將使用者與郵件服務從 Prisma 遷移至 Drizzle ORM，並為商店控制器新增匿名存取權限

--- src/stores/stores.controller.ts
+++ src/stores/stores.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { ApiOperation } from '@nestjs/swagger';
+import { AllowAnonymous } from '@thallesp/nestjs-better-auth';
 
 import { ReadMenuDto } from './dto/read-menu.dto';
 import { ReadStoreDto } from './dto/read-store.dto';
@@ -9,12 +10,14 @@ import { StoresService } from './stores.service';
 export class StoresController {
   constructor(private readonly storesService: StoresService) {}
 
+  @AllowAnonymous()
   @Get()
   @ApiOperation({ summary: '查詢所有門市' })
   findAll(): Promise<ReadStoreDto[]> {
     return this.storesService.stores({});
   }
 
+  @AllowAnonymous()
   @Get(':id/menus')
   @ApiOperation({ summary: '查詢店家菜單' })
   findAllMenus(@Param('id') id: string): Promise<ReadMenuDto[]> {
