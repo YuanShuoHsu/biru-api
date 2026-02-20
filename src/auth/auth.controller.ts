229e4861a7d43538d088b6ae21f2bcccefe4e81e
YuanShuoHsu <ook1725@gmail.com>
2026-02-20 20:30:29 +0800

    feat!: 移除重發驗證信功能及其相關 DTO

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,25 +1,8 @@
-import { Body, Controller, Post, Request } from '@nestjs/common';
-import { ApiOperation } from '@nestjs/swagger';
-
-import { fromNodeHeaders } from 'better-auth/node';
-import type { Request as ExpressRequest } from 'express';
+import { Controller } from '@nestjs/common';
 
 import { AuthService } from './auth.service';
-import { ResendEmailDto } from './dto/resend-email.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-
-  @Post('resend-email')
-  @ApiOperation({ summary: '重發驗證信' })
-  async resend(
-    @Body() resendEmailDto: ResendEmailDto,
-    @Request() req: ExpressRequest,
-  ): Promise<void> {
-    return this.authService.resendEmail(
-      resendEmailDto,
-      fromNodeHeaders(req.headers),
-    );
-  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,41 +1,6 @@
-import {
-  BadRequestException,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
-import { AuthService as BetterAuthService } from '@thallesp/nestjs-better-auth';
-
-import { I18nService } from 'nestjs-i18n';
-import { UsersService } from 'src/users/users.service';
-
-import { ResendEmailDto } from './dto/resend-email.dto';
-import type { Auth } from './index';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AuthService {
-  constructor(
-    private readonly betterAuthService: BetterAuthService<Auth>,
-    private readonly usersService: UsersService,
-    private readonly i18n: I18nService,
-  ) {}
-
-  async resendEmail(
-    { callbackURL, identifier }: ResendEmailDto,
-    headers: Headers,
-  ): Promise<void> {
-    const user = await this.usersService.user({ id: identifier });
-
-    if (!user) throw new NotFoundException(this.i18n.t('users.userNotFound'));
-
-    if (user.emailVerified)
-      throw new BadRequestException(this.i18n.t('users.emailAlreadyVerified'));
-
-    await this.betterAuthService.api.sendVerificationEmail({
-      body: {
-        callbackURL,
-        email: user.email,
-      },
-      headers,
-    });
-  }
+  constructor() {}
 }
