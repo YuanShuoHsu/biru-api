38b59321c7551a144dcfed91c78860b27849956b
許元碩 <ook1725@gmail.com>
2026-05-14 02:02:20 +0800

    refactor: add and then immediately remove url and in_language columns from menu_section table

---
+++ drizzle/0021_fast_mariko_yashida.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "menu_section" ADD COLUMN "url" text;--> statement-breakpoint
+ALTER TABLE "menu_section" ADD COLUMN "in_language" text;
---
+++ drizzle/0022_goofy_jackal.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "menu_section" DROP COLUMN "url";--> statement-breakpoint
+ALTER TABLE "menu_section" DROP COLUMN "in_language";
