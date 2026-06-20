fa6caa5ddcd68bcb2a11f6fb8d97f63c69238e8b
許元碩 <ook1725@gmail.com>
2026-06-20 12:14:56 +0800

    feat: apply AllowAnonymous decorator to LoveCodesController to permit public access

--- src/love-codes/love-codes.controller.ts
+++ src/love-codes/love-codes.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
+import { AllowAnonymous } from '@thallesp/nestjs-better-auth';
 
 import { LoveCodesService } from './love-codes.service';
 
+@AllowAnonymous()
 @Controller('love-codes')
 export class LoveCodesController {
   constructor(private readonly loveCodesService: LoveCodesService) {}
